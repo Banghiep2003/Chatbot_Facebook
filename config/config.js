d8f24c2be1cc0e504859c1013eaac74953e425a3
Banghiep2003 <115362114+Banghiep2003@users.noreply.github.com>
2023-03-31 08:22:12 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const NodeCache = require('node-cache')
 
 const config = {
     prefix: '/',
-    openaikey: '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',
+    openaikey: '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',
     cache: new NodeCache({
         checkperiod: 10000,
         deleteOnExpire: true
